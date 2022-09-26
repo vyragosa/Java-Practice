4d9fc6e3b68f0637c89528cdd8f6421a1389bd70
uumlaut <39193849+vyragosa@users.noreply.github.com>
2022-09-26 13:09:27 +0300

    updated

--- src/Ex1/Program4.java
+++ src/Ex1/Program4.java
@@ -1,4 +1,12 @@
 package Ex1;
 
 public class Program4 {
+    public static void main(String[] args) {
+        double res = 1, sum = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%.2f", res);
+            System.out.print("\t");
+            res = 1. / (i + 2);
+        }
+    }
 }
---
+++ src/Ex1/Program6.java
@@ -0,0 +1,15 @@
+package Ex1;
+
+import java.util.Scanner;
+
+public class Program6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int res = 1;
+        for(int i = 1; i<=n; i++) {
+            res *=i;
+        }
+        System.out.println(res);
+    }
+}
