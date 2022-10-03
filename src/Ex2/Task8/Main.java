936599c0686f58f5627b2240efbbd212b29f5a52
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-03 22:03:11 +0300

    Update Main.java

--- src/Ex2/Task8/Main.java
+++ src/Ex2/Task8/Main.java
@@ -1,17 +1,10 @@
 package Ex2.Task8;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-	public static String reverse(String[] arr) {
-		for (int i = 0; i < arr.length / 2; i++) {
-			String temp = arr[i];
-			arr[i] = arr[arr.length - i - 1];
-			arr[arr.length - i - 1] = temp;
-		}
-		return Arrays.toString(arr);
-	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -20,8 +13,7 @@ public class Main {
 		for (int i = 0; i < arr.length; i++)
 			arr[i] = scanner.next();
 		System.out.println(Arrays.toString(arr));
+		Collections.reverse(Arrays.asList(arr));
 
-		//Collections.reverse(Arrays.asList(arr));
-		System.out.println(reverse(arr));
 	}
 }
