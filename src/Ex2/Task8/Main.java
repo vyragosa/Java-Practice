823bee4c1cbb67005a8e12ece3b9f69e195564c1
uumlaut <39193849+vyragosa@users.noreply.github.com>
2022-10-04 17:04:43 +0300

    Update Main.java

--- src/Ex2/Task8/Main.java
+++ src/Ex2/Task8/Main.java
@@ -14,6 +14,6 @@ public class Main {
 			arr[i] = scanner.next();
 		System.out.println(Arrays.toString(arr));
 		Collections.reverse(Arrays.asList(arr));
-
+		System.out.println(Arrays.toString(arr));
 	}
 }
