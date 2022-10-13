596bedb539c31a68d9b969069b21fe1603f32845
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-13 16:06:08 +0300

    Finished 10 practice

---
+++ src/Practice10/Task3/Student.java
@@ -0,0 +1,53 @@
+package Practice10.Task3;
+
+class Student implements Comparable<Student> {
+	private final String firstName;
+	private final String lastName;
+	private final int specialty;
+	private final int group;
+	private final int GPA;
+
+	public Student(String firstName, String lastName, int specialty, int group, int gpa) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.specialty = specialty;
+		this.group = group;
+		GPA = gpa;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getSpecialty() {
+		return specialty;
+	}
+
+	public int getGroup() {
+		return group;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", specialty=" + specialty +
+				", group=" + group +
+				", GPA=" + GPA +
+				'}';
+	}
+
+	public int getGPA() {
+		return GPA;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		return this.group - o.group;
+	}
+}
