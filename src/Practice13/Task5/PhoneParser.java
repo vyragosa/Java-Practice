1ee754aa8a3f1852e3ca03bd3346c95fd348a106
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-16 01:07:39 +0300

    small fix

--- src/Practice13/Task5/PhoneParser.java
+++ src/Practice13/Task5/PhoneParser.java
@@ -4,7 +4,7 @@ class PhoneParser {
 	String formatNumber;
 
 	public PhoneParser(String number) {
-		this.formatNumber = "+%s-%s-%s".formatted(number.substring(0, number.length() - 7),
+		this.formatNumber = "%s-%s-%s".formatted(number.substring(0, number.length() - 7),
 				number.substring(number.length() - 7, number.length() - 4),
 				number.substring(number.length() - 4));
 		if (number.charAt(0) != '+') this.formatNumber = "" + formatNumber;
