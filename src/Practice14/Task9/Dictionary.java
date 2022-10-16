afbdfe199df8e712dee6134e4bebf087e2e3ad92
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-16 18:05:03 +0300

    Long line fix

--- src/Practice14/Task9/Dictionary.java
+++ src/Practice14/Task9/Dictionary.java
@@ -13,6 +13,10 @@ public class Dictionary {
 	}
 
 	public static void main(String[] args) {
-		MakeDictionary("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+		MakeDictionary("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut" +
+				" labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
+				" ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+				"cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa" +
+				" qui officia deserunt mollit anim id est laborum.");
 	}
 }
