a920e175d98a95f45f6633c76698825b8919b3eb
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-19 12:44:29 +0300

    Update Main.java

--- src/Practice20/Task6/Main.java
+++ src/Practice20/Task6/Main.java
@@ -2,10 +2,10 @@ package Practice20.Task6;
 
 class Main {
 	public static void main(String[] args) {
-		Matrix<Integer> tmp = new Matrix<>();
-		tmp.setMatrix(new Integer[][]{{1, 2}, {3, 4}});
-		tmp.printMatrix();
-		tmp.transparentMatrix();
-		tmp.printMatrix();
+		Matrix<Integer> integerMatrix = new Matrix<>();
+		integerMatrix.setMatrix(new Integer[][]{{1, 2}, {3, 4}});
+		integerMatrix.printMatrix();
+		integerMatrix.transparentMatrix();
+		integerMatrix.printMatrix();
 	}
 }
