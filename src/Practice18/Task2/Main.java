5e0bf2247e432202c6adad885f289c37eba0dec0
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-19 19:35:37 +0300

    some fixes

--- src/Practice18/Task2/Main.java
+++ src/Practice18/Task2/Main.java
@@ -2,7 +2,7 @@ package Practice18.Task2;
 
 class Main {
 	public static void main(String[] args) {
-		Practice18.Task4.Exception2 exception2 = new Exception2();
+		Exception2 exception2 = new Exception2();
 		exception2.exceptionDemo();
 	}
 }
--- src/Practice18/Task3/Main.java
+++ src/Practice18/Task3/Main.java
@@ -2,7 +2,7 @@ package Practice18.Task3;
 
 class Main {
 	public static void main(String[] args) {
-		Practice18.Task4.Exception2 exception2 = new Exception2();
+		Exception2 exception2 = new Exception2();
 		exception2.exceptionDemo();
 	}
 }
