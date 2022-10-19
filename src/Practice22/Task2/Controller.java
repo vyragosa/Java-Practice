5821fec16fce413604d89504e65a5a67aec4ca78
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-19 22:51:17 +0300

    Finished 22 practice

---
+++ src/Practice22/Task2/Controller.java
@@ -0,0 +1,28 @@
+package Practice22.Task2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    private Viewer viewer;
+    private Model model;
+
+    public Controller() {
+        model = new Model();
+        this.viewer = new Viewer();
+    }
+
+    public void execute() {
+        viewer.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    model.doAction(e.getActionCommand());
+                } catch (Exception ex) {
+                    viewer.errorMessage(ex.getMessage());
+                }
+                viewer.update(model.getTemp());
+            }
+        });
+    }
+}
---
+++ src/Practice22/Task2/Driver.java
@@ -0,0 +1,8 @@
+package Practice22.Task2;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		new Controller().execute();
+	}
+}
