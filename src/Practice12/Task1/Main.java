4f8da0680d40e03bee9d3a1be63b45f8cda2a39e
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-22 23:48:03 +0300

    Code improvements

--- src/Practice12/Task1/Main.java
+++ src/Practice12/Task1/Main.java
@@ -2,6 +2,6 @@ package Practice12.Task1;
 
 class Main {
 	public static void main(String[] args) {
-		DrawGUI frame = new DrawGUI();
+		new DrawGUI();
 	}
 }
--- src/Practice5/Task2/Main.java
+++ src/Practice5/Task2/Main.java
@@ -2,6 +2,6 @@ package Practice5.Task2;
 
 class Main {
 	public static void main(String[] args) {
-		DrawGUI frame = new DrawGUI();
+		new DrawGUI();
 	}
 }
